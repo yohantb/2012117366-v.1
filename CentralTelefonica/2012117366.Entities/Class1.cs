90949283a618cdb12aa61f76f57e84743f61b75c
yohantb <you@example.com>
2017-04-09 19:43:23 -0500

    segundo commit

---
+++ CentralTelefonica/2012117366.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012117366.Entities
+{
+    public class Class1
+    {
+    }
+}
